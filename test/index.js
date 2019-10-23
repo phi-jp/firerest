7b71536448fd410d18b092a07f8cc6cc394b61b1
phi <phi1618jp@gmail.com>
2019-10-24 01:48:02 +0900

    fetch の前に Promise 系の処理を挟めるよう修正

--- test/index.js
+++ test/index.js
@@ -7,6 +7,16 @@ var ref = Firerest.create({
   debug: true,
 });
 
+// fetch 前に共通処理を仕込める( Promise にも対応 )
+ref.on('prefetch', () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+      console.log('共通に2秒待ってから表示する')
+    }, 2000);
+  });
+});
+
 ref.child('posts').get().then(function(res) {
-  console.log(res);
+  console.log(res.slice(0, 4));
 });
